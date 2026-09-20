c0deada7f34e17302246a202bd40e5cdeb021951
sejalyadav-8788 <sejalyadav20031516@gmail.com>
2026-09-20 09:54:10 +0530

    Fix CORS for deployed frontend

--- src/main/java/com/campusResolve/backend/security/securityConfig.java
+++ src/main/java/com/campusResolve/backend/security/securityConfig.java
@@ -35,10 +35,7 @@ public class securityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
-        ));
+        configuration.setAllowedOrigins(List.of("*"));
 
         configuration.setAllowedMethods(List.of(
                 "GET",
